8ef052a958f3c6830bfd87ab8c2b41db3acfe75b
Simon <Simon.meersschaut@gmail.com>
2021-09-18 09:55:48 +0200

    password foem

--- main.py
+++ main.py
@@ -5,9 +5,31 @@ from selenium.webdriver.common.keys import Keys
 import time
 import threading
 import pymsgbox
+import json
+import os
 
 def message():
   pymsgbox.alert('Trade found!', 'Title')
+
+USERNAME_ERROR = '!Fill in the username form under .gitignore and restart the program!'
+try:
+  with open('.gitignore/user.json', 'r') as f:
+    content = json.load(f)
+    email = content['email']
+    password = content['password']
+    if email == 'put your email here' or password == 'put your password here':
+      input(USERNAME_ERROR)
+      exit()
+except FileNotFoundError:
+  try:
+    os.mkdir('.gitignore')
+  except FileExistsError:
+    pass
+  _ = open('.gitignore/user.json', "w")
+  with open('.gitignore/user.json', 'w') as f:
+    f.write('{"email":"put your email here", "password":"put your password here"}')
+  input(USERNAME_ERROR)
+  exit()
 
 chat_message = "I'm interested"
 max_credits = 400
@@ -43,7 +65,7 @@ def start_checking():
       text = card.find_element_by_class_name('rlg-trade__time').find_elements_by_css_selector("*")[0].get_attribute('innerHTML')
       print(card.find_element_by_class_name('rlg-trade__time').get_attribute('innerHTML'))
       print(text)
-      if 'minute' in text and int(text.split(' ')[0]) < max_minutes:
+      if not('minute' in text) or int(text.split(' ')[0]) > max_minutes:
         return None
 
 def send_message():
@@ -58,8 +80,8 @@ def send_message():
 def login():
   
   driver.find_element_by_xpath('/html/body/header/section[1]/div/div[2]/div/a[1]').click()
-  driver.find_element_by_xpath('/html/body/main/main/section/div/div/div[1]/form/input[2]').send_keys('simon.broodje.kaas@gmail.com')
-  driver.find_element_by_xpath('/html/body/main/main/section/div/div/div[1]/form/input[3]').send_keys('simonnomis8')
+  driver.find_element_by_xpath('/html/body/main/main/section/div/div/div[1]/form/input[2]').send_keys(email)
+  driver.find_element_by_xpath('/html/body/main/main/section/div/div/div[1]/form/input[3]').send_keys(password)
   time.sleep(.5)
   driver.find_element_by_xpath('/html/body/main/main/section/div/div/div[1]/form/input[4]').click()
 
@@ -90,15 +112,7 @@ def setup():
   
   #card.click()
   
-  
-
 
 if __name__ == '__main__':
   driver = webdriver.Chrome()
   setup()
-
-#time.sleep(5)
-
-
-
-#driver.close()
